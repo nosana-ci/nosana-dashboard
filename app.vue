c87811c0bf9c2b18af1595afd06da23d57ed583b
Laurens Verspeek <l.verspeek@gmail.com>
2024-01-26 16:40:53 +0100

    fix og meta tags

--- app.vue
+++ app.vue
@@ -5,3 +5,31 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Nosana Dashboard',
+  ogTitle: 'Nosana Dashboard',
+  twitterTitle: 'Nosana Dashboard',
+  description: 'Powering the AI revolution',
+  ogDescription: 'Powering the AI revolution',
+  twitterDescription: 'Powering the AI revolution',
+  ogImage: 'https://app.nosana.com/img/screenshot.png',
+  twitterImage: 'https://app.nosana.com/img/screenshot.png',
+  twitterCard: 'summary_large_image'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/ico',
+      href: '/favicon.ico'
+    }
+  ]
+})
+
+</script>
