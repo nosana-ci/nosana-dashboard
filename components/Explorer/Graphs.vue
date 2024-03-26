9c11ca766b5e8fe856130d83278a2eeafcdca6b0
Laurens Verspeek <l.verspeek@gmail.com>
2024-03-26 23:53:00 +0100

    fix graph

--- components/Explorer/Graphs.vue
+++ components/Explorer/Graphs.vue
@@ -7,17 +7,27 @@
             <h2 class="title is-5 mb-0">
               INFERENCES
               <span class="has-text-weight-bold ml-2">
-                <count-up v-if="jobs" :end-val="jobs.length"></count-up>
+                <count-up v-if="jobs" :end-val="jobData.datasets[0].data.reduce((n, { y }) => n + y, 0)"></count-up>
                 <span v-else-if="loadingJobs">...</span>
                 <span v-else>-</span>
               </span>
+              <small class="ml-2">
+                <span v-if="time == 1 * 3600">(last hour)</span>
+                <span v-else-if="time == 24 * 3600">(last day)</span>
+                <span v-else-if="time == 7 * 24 * 3600">(last week)</span>
+                <span v-else-if="time == 365 / 12 * 24 * 3600">(last month)</span>
+                <span v-else-if="time == 365 / 4 * 24 * 3600">(last quarter)</span>
+                <span v-else-if="time == 365 * 24 * 3600">(last year)</span>
+              </small>
             </h2>
 
             <div class="select is-small ml-auto">
               <select v-model="time">
                 <option :value="1 * 3600">1h</option>
                 <option :value="24 * 3600">24h</option>
+                <option :value="7 * 24 * 3600">7d</option>
                 <option :value="365 / 12 * 24 * 3600">1M</option>
+                <option :value="365 / 4 * 24 * 3600">3M</option>
                 <option :value="365 * 24 * 3600">1Y</option>
                 <option :value="0">All</option>
               </select>
@@ -65,7 +75,7 @@ declare module 'chart.js' {
   }
 }
 
-const { data: jobs, pending: loadingJobs } = await useAPI('/api/jobs');
+const { data: jobs, pending: loadingJobs } = await useAPI('/api/jobs/stats');
 Interaction.modes.interpolate = Interpolate;
 
 Chartjs.register(
@@ -82,20 +92,19 @@ Chartjs.register(
   CrosshairPlugin,
 );
 
-const time: Ref<number | string> = ref(365 * 24 * 3600);
+const time: Ref<number | string> = ref(365 / 12 * 24 * 3600); // 1 month
 
 const tooltipFormat = ref('[Week] W MMM YYYY');
 const unit: Ref<false | "millisecond" | "second" | "minute" | "hour" | "day" | "week" | "month" | "quarter" | "year" | undefined> = ref('day');
 
 const jobData = computed<ChartData<'line'>>(() => {
-  const data: Array<any> = jobs.value ? [...jobs.value] : [];
+  const data: Array<any> = jobs.value && jobs.value.timeStamps ? [...jobs.value.timeStamps] : [];
   const updatedData: Array<any> = [];
   const tempDateCollection: Array<any> = [];
-
   data
-    .sort((a, b) => b.timeStart - a.timeStart)
+    .sort((a, b) => b - a)
     .forEach((j) => {
-      const timestamp = j.timeStart * 1000;
+      const timestamp = j * 1000;
       const timeValue = parseInt(time.value as string);
       if (timestamp && (!timeValue || timestamp > Date.now() - timeValue * 1000)) {
         const formatedDate = dayjs(timestamp).format('MMM/YYYY');
@@ -107,26 +116,31 @@ const jobData = computed<ChartData<'line'>>(() => {
 
         const weekNumber = Math.ceil(days / 7);
         let granularity: string | null;
-        if (timeValue > 365 / 12 * 24 * 3600) {
-          // Bigger than 1 month, group by week
+        if (timeValue > 365 / 3 * 24 * 3600) {
+          // Bigger than 4 month, group by week
           granularity = weekNumber + dayjs(timestamp).format('YYYY');
-          tooltipFormat.value = '[Week] W MMM YYYY';
+          tooltipFormat.value = '[Week] W - MMMM YYYY';
           unit.value = 'month';
-        } else if (timeValue > 3 * 24 * 3600) {
-          // Bigger than 3 days, group by day
+        } else if (timeValue > 5 * 24 * 3600) {
+          // Bigger than 5 days, group by day
           granularity = dayjs(timestamp).format('DD/MMM/YYYY');
-          tooltipFormat.value = 'DD MMM YYYY';
+          tooltipFormat.value = 'DD MMMM YYYY';
           unit.value = 'day';
         } else if (timeValue > 12 * 3600) {
-          // Bigger than 12 hours, group by day
-          granularity = dayjs(timestamp).format('DD/MMM/YYYY');
-          tooltipFormat.value = 'DD MMM YYYY';
-          unit.value = 'day';
+          // Bigger than 12 hours, group by hour
+          granularity = dayjs(timestamp).format('HH DD/MMM/YYYY');
+          tooltipFormat.value = 'HH[h] - DD MMMM YYYY';
+          unit.value = 'hour';
+        } else if (timeValue > 0) {
+          // under 12 hours
+          granularity = dayjs(timestamp).format('HH:mm DD/MMM/YYYY');
+          tooltipFormat.value = 'HH:mm - DD MMMM YYYY';
+          unit.value = 'minute';
         }
         else {
           // All: Group by month
           granularity = dayjs(timestamp).format('MMM/YYYY');
-          tooltipFormat.value = 'MMM YYYY';
+          tooltipFormat.value = 'MMMM YYYY';
           unit.value = 'month';
         }
         if (granularity && tempDateCollection.includes(granularity)) {
