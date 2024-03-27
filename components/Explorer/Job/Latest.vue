be94c1ca10b7f0545397b52fe6b485b6e229a3f0
Laurens Verspeek <l.verspeek@gmail.com>
2024-03-27 16:52:09 +0100

    move to latest block

---
+++ components/Explorer/Job/Latest.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ExplorerJobList :per-page="limit" :total-jobs="limit" v-model:page="page" v-model:state="state"
+      :loading-jobs="loadingJobs" title="Latest Inferences" :jobs="jobs ? jobs.jobs : null" :small="true">
+    </ExplorerJobList>
+    <div class="has-text-right mt-auto pt-2">
+      <nuxt-link to="/jobs" class="button is-white">
+        <span>All jobs</span>
+        <span class="icon"> &#8250; </span>
+      </nuxt-link>
+    </div>
+    <div v-if="!loadingJobs && !jobs">Could not load jobs</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const page: Ref<number> = ref(1);
+const state: Ref<number | null> = ref(null);
+const jobStateMapping: any = {
+  0: 'QUEUED',
+  1: 'RUNNING',
+  2: 'COMPLETED',
+  3: 'STOPPED',
+};
+const limit: Ref<number> = ref(10);
+const jobsUrl = computed(() => { return `/api/jobs?limit=${limit.value}&offset=${(page.value - 1) * limit.value}${state.value !== null ? `&state=${jobStateMapping[state.value]}` : ''}` })
+watch(jobsUrl, () => {
+  console.log('resetting jobs..')
+  jobs.value = null
+})
+const { data: jobs, pending: loadingJobs, refresh: refreshJobs } = await useAPI(jobsUrl, { watch: [jobsUrl] });
+
+// Fetch jobs every 30 seconds
+useIntervalFn(refreshJobs, 30000);
+</script>
--- pages/explorer.vue
+++ pages/explorer.vue
@@ -17,14 +17,8 @@
     <div class="columns mt-4 is-multiline">
       <div class="column is-6">
         <div class="box is-flex is-flex-direction-column">
-          <ExplorerJobList :per-page="limit" :total-jobs="limit" v-model:page="page" v-model:state="state" :loading-jobs="loadingJobs" title="Latest Inferences" :jobs="jobs ? jobs.jobs : null" :small="true">
-          </ExplorerJobList>
-          <div class="has-text-right mt-auto pt-2">
-            <nuxt-link to="/jobs" class="button is-white">
-              <span>All jobs</span>
-              <span class="icon"> &#8250; </span>
-            </nuxt-link>
-          </div>
+          <ExplorerJobLatest />
+          
         </div>
       </div>
       <div class="column is-6">
@@ -40,27 +34,10 @@
         </div>
       </div>
     </div>
-    <div v-if="!loadingJobs && !jobs">Could not load jobs</div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { WalletMultiButton } from "solana-wallets-vue";
 const config = useRuntimeConfig();
-const page: Ref<number> = ref(1);
-const state: Ref<number | null> = ref(null);
-const jobStateMapping: any = {
-  0: 'QUEUED',
-  1: 'RUNNING',
-  2: 'COMPLETED',
-  3: 'STOPPED',
-};
-const limit: Ref<number> = ref(5);
-const jobsUrl = computed(() => { return `/api/jobs?limit=${limit.value}&offset=${(page.value - 1) * limit.value}${state.value !== null ? `&state=${jobStateMapping[state.value]}` : '' }` })
-watch(jobsUrl, () => { 
-  console.log('resetting jobs..')
-  jobs.value = null
-})
-const { data: jobs, pending: loadingJobs } = await useAPI(jobsUrl, {watch: [jobsUrl]});
-
 </script>
