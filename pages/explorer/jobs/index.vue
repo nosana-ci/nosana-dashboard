96b1b9f52fa8b57412b97a5317476a8333862d1f
Laurens Verspeek <l.verspeek@gmail.com>
2024-03-28 12:39:34 +0100

    add jobs page

--- pages/explorer/jobs/index.vue
+++ pages/explorer/jobs/index.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-    <ExplorerJobList :per-page="limit" :total-jobs="limit" v-model:page="page" v-model:state="state"
-      :loading-jobs="loadingJobs" title="Latest Inferences" :jobs="jobs ? jobs.jobs : null" :small="true">
+    <div class="box">
+      <ExplorerJobList :per-page="limit" :total-jobs="jobs ? jobs.totalJobs : null" v-model:page="page" v-model:state="state"
+      :loading-jobs="loadingJobs" :jobs="jobs ? jobs.jobs : null">
     </ExplorerJobList>
-    <div class="has-text-right mt-auto pt-2">
-      <nuxt-link to="/explorer/jobs" class="button is-white">
-        <span>All jobs</span>
-        <span class="icon"> &#8250; </span>
-      </nuxt-link>
     </div>
     <div v-if="!loadingJobs && !jobs">Could not load jobs</div>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
 const page: Ref<number> = ref(1);
 const state: Ref<number | null> = ref(null);
 const jobStateMapping: any = {
@@ -22,7 +18,7 @@ const jobStateMapping: any = {
   2: 'COMPLETED',
   3: 'STOPPED',
 };
-const limit: Ref<number> = ref(10);
+const limit: Ref<number> = ref(17);
 const jobsUrl = computed(() => { return `/api/jobs?limit=${limit.value}&offset=${(page.value - 1) * limit.value}${state.value !== null ? `&state=${jobStateMapping[state.value]}` : ''}` })
 watch(jobsUrl, () => {
   console.log('resetting jobs..')
