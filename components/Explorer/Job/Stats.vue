5ec4e59919133e96c58809b50c565544bb7e5624
Laurens Verspeek <l.verspeek@gmail.com>
2024-08-22 19:57:19 +0200

    countup

--- components/Explorer/Job/Stats.vue
+++ components/Explorer/Job/Stats.vue
@@ -6,43 +6,35 @@
         <h2 class="subtitle">Completed jobs</h2>
         <h2 class="title is-1 pt-2">
           <span v-if="loadingStats">...</span>
-          <span v-else>{{ stats.completed }}</span>
+          <CustomCountUp v-else :end-val="stats.completed"></CustomCountUp>
         </h2>
       </div>
       <div class="column is-one-fifth">
         <h2 class="subtitle">Running jobs</h2>
         <h2 class="title is-1 pt-2">
           <span v-if="loadingJobs">...</span>
-          <span v-else>{{ runningJobs.totalJobs }}</span>
+          <CustomCountUp v-else :end-val="runningJobs.totalJobs"></CustomCountUp>
         </h2>
       </div>
       <div class="column is-one-fifth">
         <h2 class="subtitle">Total NOS earned</h2>
         <h2 class="title is-1 pt-2">
           <span v-if="loadingStats">...</span>
-          <span v-else>{{ Math.round(stats.price) }}</span>
+          <CustomCountUp v-else :end-val="Math.round(stats.price)"></CustomCountUp>
         </h2>
       </div>
       <div class="column is-one-fifth">
         <h2 class="subtitle">Total inference hours</h2>
         <h2 class="title is-1 pt-2">
           <span v-if="loadingStats">...</span>
-          <span v-else>{{ Math.round(stats.duration / 3600) }}</span>
+          <CustomCountUp v-else :end-val="Math.round(stats.duration / 3600)"></CustomCountUp>
         </h2>
       </div>
       <div class="column is-one-fifth">
         <h2 class="subtitle">Online GPU Nodes</h2>
         <h2 class="title is-1 pt-2">
           <span v-if="loadingMarkets || loadingJobs">...</span>
-          <span v-else-if="markets">
-            {{
-            markets
-              .reduce(
-                (a, b) => a + (b.queueType === 1 ? b.queue.length : 0),
-                0,
-              ) + runningJobs.totalJobs
-            }}
-          </span>
+          <CustomCountUp v-else-if="markets" :end-val="markets.reduce((a, b) => a + (b.queueType === 1 ? b.queue.length : 0), 0) + runningJobs.totalJobs"></CustomCountUp>
         </h2>
       </div>
     </div>
