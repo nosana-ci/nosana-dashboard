b57abe8376f64c5a2282ed69f9485e7fc457fccb
Laurens Verspeek <l.verspeek@gmail.com>
2024-10-01 13:22:31 +0200

    add template filters

--- pages/jobs/templates.vue
+++ pages/jobs/templates.vue
@@ -1,36 +1,97 @@
 <template>
   <div>
     <TopBar :title="'Job Builder'" :subtitle="'Create and deploy job definition files'"></TopBar>
-    <div class="box has-background-white-ter">
-      <h2 class="title">Set up your Job</h2>
-      <h3 class="subtitle">Select a template to get started or start with a blank template.</h3>
-      <div v-for="category in categories" class="mb-6">
-        <h2 class="title is-4">{{ category }}</h2>
-        <h3 class="subtitle is-6">{{ templates.filter(t => t.category.includes(category)).length }} templates</h3>
-        <div class="columns is-multiline">
-          <div v-for="template in templates.filter(t => t.category.includes(category))" :key="template.name"
-            class="column is-4">
-            <nuxt-link class="box" style="height: 100%;"
-              :to="{ path: '/jobs/create', query: { templateId: template.id } }">
-              <div class="is-flex is-justify-content-space-between">
-                <h2 class="is-size-4 has-text-weight-semibold mb-0 has-text-black">
-                  {{ template.name }}
-                </h2>
-                <div v-if="template.icon" class="template-icon is-flex has-text-centered has-background-white">
-                  <img :src="template.icon">
+    <h2 class="title">Explore our Templates</h2>
+    <h3 class="subtitle">Select a template to get started with a pre-built Job Definition File</h3>
+    <div class="columns">
+      <div class="column is-3">
+        <div class="box has-background-white-ter">
+          <h2 class="title is-4">Filters</h2>
+          <div class="field">
+            <div class="control">
+              <label class="label">Search</label>
+              <input class="input" type="text" v-model="search" placeholder="Search templates" />
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <label class="label">Category</label>
+              <div class="menu">
+                <ul class="menu-list">
+                  <li>
+                    <a @click="filterCategory = null" :class="{ 'is-active': filterCategory === null }">
+                      <span>All</span>
+                      <span class="ml-auto is-size-7">({{ templates.length }})</span>
+                    </a>
+
+                  </li>
+                  <li v-for="category in categories">
+                    <a :class="{ 'is-active': filterCategory === category }" @click="filterCategory = category">
+                      <span>{{ category }}</span>
+                      <span class="ml-auto is-size-7">({{ templates.filter(t =>
+                        t.category.includes(category)).length }})</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="column is-9">
+        <div class="box has-background-white-ter">
+
+          <div v-for="category in categories">
+            <div v-if="!filterCategory || category === filterCategory" class="mb-6">
+              <h2 class="title is-4">{{ category }}</h2>
+              <h3 class="subtitle is-6">{{ filteredTemplates.filter(t => t.category.includes(category)).length }}
+                templates</h3>
+              <div class="columns is-multiline">
+                <div v-for="template in filteredTemplates.filter(t => t.category.includes(category))"
+                  :key="template.name" class="column is-4">
+                  <nuxt-link class="box" style="height: 100%;"
+                    :to="{ path: '/jobs/create', query: { templateId: template.id } }">
+                    <div class="is-flex is-justify-content-space-between">
+                      <h2 class="is-size-4 has-text-weight-semibold mb-0 has-text-black">
+                        {{ template.name }}
+                      </h2>
+                      <div v-if="template.icon" class="template-icon is-flex has-text-centered has-background-white">
+                        <img :src="template.icon">
+                      </div>
+                    </div>
+                    <p>{{ template.description }}</p>
+                  </nuxt-link>
                 </div>
               </div>
-              <p>{{ template.description }}</p>
-            </nuxt-link>
+            </div>
           </div>
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
 <script lang="ts" setup>
 const { templates } = useTemplates();
+const filterCategory: Ref<string | null> = ref(null);
+const search: Ref<string | null> = ref(null);
+const filteredTemplates = computed(() => {
+  return templates.value.filter(t => {
+    let found = true;
+    let inCategory = true;
+    if (search.value) {
+      found = t.name.toLowerCase().includes(search.value.toLowerCase()) ||
+        t.description.toLowerCase().includes(search.value.toLowerCase()) ||
+        t.readme.toLowerCase().includes(search.value.toLowerCase());
+    }
+    if (filterCategory.value) {
+      inCategory = t.category.includes(filterCategory.value);
+    }
+    return found && inCategory;
+  })
+});
 const categories = computed(() => {
   const categoryIds = [...new Set(templates.value.flatMap(({ category }) => category))];
 
