d61ccd6bac986eae2ef9416801cf612116a89d0d
Laurens Verspeek <l.verspeek@gmail.com>
2025-01-09 19:56:53 +0100

    upgrade grid block

--- components/Staking/Testgrid.vue
+++ components/Staking/Testgrid.vue
@@ -2,19 +2,18 @@
   <div class="dark-mode">
     <div class="box has-background-body mb-5" style="background-color: #051306 !important;">
       <logo width="130px" class="mt-3" :white="true" :animated="true" />
-      <h2 class="title is-2 pt-2 mb-3 is-family-primary has-text-weight-semibold">
-        Test Grid Specifications
+      <h2 class="title is-2 pt-2 mb-4 has-text-weight-semibold">
+        AI innovation happens on Nosana
       </h2>
-      <a class="button is-outlined is-wider is-secondary has-text-weight-semibold is-rounded"
-        href="https://nosana.com/compute" target="_blank">
-        Find Out More
+      <a class="button is-outlined is-widest is-white is-rounded" href="https://nosana.com/" target="_blank">
+        Explore our Solutions
       </a>
     </div>
   </div>
 </template>
 <style scoped lang="scss">
 .box {
-  background-image: url('@/assets/img/testgrid.svg');
+  background-image: url('@/assets/img/grid.jpg');
   background-position: right;
   background-size: contain;
   background-repeat: no-repeat;
