11cf29d9bb8c6b65c63145801d5940ec3a17bd5b
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-04-07 17:01:50 +0000

    fix dashboard search bar

--- components/Explorer/Search.vue
+++ components/Explorer/Search.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="field has-addons">
-    <div class="control is-fullwidth dropdown is-active has-icons-left" :class="{ 'is-loading': checkingIfJob }">
-      <input v-model="address" autofocus type="text" class="input" style="
+    <div
+      class="control is-fullwidth dropdown is-active has-icons-left"
+      :class="{ 'is-loading': checkingIfJob }"
+    >
+      <input
+        v-model="address"
+        autofocus
+        type="text"
+        class="input"
+        style="
           padding-top: 1.4rem !important;
           padding-bottom: 1.4rem !important;
-        " placeholder="Search for deployements, hosts and GPUs" :disabled="checkingIfJob" />
+        "
+        placeholder="Search for deployements, hosts and GPUs"
+        :disabled="checkingIfJob"
+      />
       <span class="icon pt-1 is-left" v-if="!checkingIfJob">
-        <SearchIcon style="width:1.5em; height: 1.5em" />
+        <SearchIcon style="width: 1.5em; height: 1.5em" />
       </span>
-      <div v-if="searchItems.length" class="dropdown-menu is-active is-fullwidth" role="menu">
+      <div
+        v-if="searchItems.length"
+        class="dropdown-menu is-active is-fullwidth"
+        role="menu"
+      >
         <div class="dropdown-content has-background-white-bis">
-          <a v-for="(item, index) in searchItems" :key="item"
+          <a
+            v-for="(item, index) in searchItems"
+            :key="item"
             class="dropdown-item px-4 py-2 is-size-6 is-flex is-justify-content-space-between"
-            :class="{ 'is-active': index === activeSearchItem }" @click="selectItem(item), (address = '')">
+            :class="{ 'is-active': index === activeSearchItem }"
+            @click="(selectItem(item), (address = ''))"
+          >
             {{ item.value }}
             <span class="is-capitalized has-text-grey-light">{{
               item.type
@@ -24,50 +43,52 @@
   </div>
 </template>
 <script setup lang="ts">
-import { type Node } from '@nosana/sdk';
-import { onKeyStroke } from '@vueuse/core';
-import { PublicKey } from '@solana/web3.js';
-import SearchIcon from '@/assets/img/icons/search.svg?component';
+import { type Node } from "@nosana/sdk";
+import { onKeyStroke } from "@vueuse/core";
+import { PublicKey } from "@solana/web3.js";
+import SearchIcon from "@/assets/img/icons/search.svg?component";
 
 const router = useRouter();
-const address = ref('');
+const address = ref("");
 const { markets } = useMarkets();
 const items: Ref<Array<any>> = ref([]);
 const activeSearchItem: Ref<number> = ref(0);
 const checkingIfJob = ref(false);
 
 const selectItem = async (item: { type: string; value: string }) => {
-  let s = '';
-  if (item.type === 'account') {
+  let s = "";
+  if (item.type === "account") {
     checkingIfJob.value = true;
     let isJob = false;
-    
+
     try {
       // @ts-ignore TODO: add to useAPI opts type
-      const { data: job } = await useAPI(`/api/jobs/${item.value}`, { disableToastOnError: true });
+      const { data: job } = await useAPI(`/api/jobs/${item.value}`, {
+        disableToastOnError: true,
+      });
       if (job.value) {
-        item.type = 'job';
+        item.type = "job";
         isJob = true;
       }
     } catch (error) {
       // Job check failed, continue to node check
     }
-    
+
     checkingIfJob.value = false;
-    
+
     // If not a job, default to treating it as a node/host
     if (!isJob) {
-      item.type = 'node';
+      item.type = "node";
     }
   }
-  
+
   // Add plural 's' except for job and node
-  if (item.type !== 'account' && item.type !== 'job' && item.type !== 'node') {
-    s = 's'
+  if (item.type !== "account" && item.type !== "node") {
+    s = "s";
   }
-  
+
   // Special case for nodes - route to /nodes/address instead of /node/address
-  if (item.type === 'node') {
+  if (item.type === "node") {
     router.push(`/nodes/${item.value}`);
   } else {
     router.push(`/${item.type}${s}/${item.value}`);
@@ -76,15 +97,15 @@ const selectItem = async (item: { type: string; value: string }) => {
 
 const searchItems = computed(() => {
   activeSearchItem.value = 0;
-  if (address.value === '') {
+  if (address.value === "") {
     return [];
   }
 
   // combine jobs and markets in one list
   items.value = markets.value
     ? markets.value!.map((a: any) => {
-      return { value: a.address.toString(), type: 'market' };
-    })
+        return { value: a.address.toString(), type: "market" };
+      })
     : [];
 
   let matches = 0;
@@ -107,31 +128,31 @@ const searchItems = computed(() => {
   if (results.length === 0) {
     try {
       const pk = new PublicKey(address.value);
-      results.push({ value: pk.toString(), type: 'account' });
-    } catch (error) { }
+      results.push({ value: pk.toString(), type: "account" });
+    } catch (error) {}
   }
 
   return results;
 });
 
 // navigate through autocomplete search with arrow keys
-onKeyStroke(['ArrowDown', 'ArrowUp', 'Enter'], (e) => {
+onKeyStroke(["ArrowDown", "ArrowUp", "Enter"], (e) => {
   e.preventDefault();
   handleKeyStroke(e);
 });
 
 const handleKeyStroke = (e: any) => {
   if (searchItems.value.length > 0) {
-    if (e.key === 'ArrowUp' && activeSearchItem.value > 0) {
+    if (e.key === "ArrowUp" && activeSearchItem.value > 0) {
       activeSearchItem.value--;
     } else if (
-      e.key === 'ArrowDown' &&
+      e.key === "ArrowDown" &&
       activeSearchItem.value < searchItems.value.length - 1
     ) {
       activeSearchItem.value++;
-    } else if (e.key === 'Enter') {
+    } else if (e.key === "Enter") {
       selectItem(searchItems.value[activeSearchItem.value]);
-      address.value = '';
+      address.value = "";
       activeSearchItem.value = 0;
     }
   }
