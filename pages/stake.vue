94a5aa2d27b74ffe40a7ade4ab91dcb5b8d9d6b6
Laurens Verspeek <l.verspeek@gmail.com>
2025-10-22 19:03:08 +0200

    add notice on staking page regarding NNP-0001

--- pages/stake.vue
+++ pages/stake.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <TopBar
-      :title="'Staking'"
-      :subtitle="'Stake NOS and earn rewards'"
-      ref="topBar"
-      v-model="showSettingsModal"
-    />
+    <TopBar :title="'Staking'" :subtitle="'Stake NOS and earn rewards'" ref="topBar" v-model="showSettingsModal" />
+    <div class="notification is-warning is-light">
+      <p>
+        <strong>Notice:</strong>
+        There is an active Nosana Network Proposal (NNP-0001) to better retarget emissions for rewards. If it passes,
+        passive staking rewards
+        will sunset over a 12-month period. Read more
+        <a href="https://github.com/nosana-ci/network-proposals/blob/main/nnp/NNP-0001-tokenomics.md" target="_blank"
+          rel="noopener">here</a>.
+      </p>
+    </div>
     <div class="columns is-multiline">
       <div class="column is-12 is-6-widescreen">
         <div class="box has-background-white-ter">
