137d185957a83f34a2b729c18f732e1dce7a834c
Laurens Verspeek <l.verspeek@gmail.com>
2026-03-11 15:57:23 +0100

    add video link to first time credit claim

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -318,6 +318,28 @@
                   </div>
                 </div>
               </div>
+
+              <!-- YouTube Video Link (Campaign Mode Only) -->
+              <a
+                v-if="isCampaignMode"
+                href="https://www.youtube.com/watch?v=J9lfl2QOmhU"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="campaign-video-link"
+              >
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"
+                  />
+                </svg>
+                Watch: How to Claim Free GPU Credits on Nosana
+              </a>
             </template>
           </div>
         </div>
@@ -1124,6 +1146,25 @@ const signAuthMessage = async (walletName: string) => {
   color: $grey;
   margin-bottom: 2rem;
   line-height: 1.5;
+}
+
+.campaign-video-link {
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
+  font-size: 0.925rem;
+  font-weight: 500;
+  text-decoration: none;
+  margin-top: 1.5rem;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  svg {
+    flex-shrink: 0;
+    color: #d32f2f;
+  }
 }
 
 /* Email Form Styles */
