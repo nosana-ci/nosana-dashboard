fa31f0f42a98bf4d09189272692a1fc3b82e6086
Jair Zijp <jair@nosana.io>
2026-08-18 09:58:59 +0000

    chore: add new GA event triggers

--- app/composables/useBuyCreditsModal.ts
+++ app/composables/useBuyCreditsModal.ts
@@ -1,9 +1,12 @@
+import { trackEvent } from "~/utils/analytics";
+
 export const useBuyCreditsModal = () => {
   const isOpen = useState("buyCreditsModalOpen", () => false);
   const purchasedTick = useState("buyCreditsPurchasedTick", () => 0);
 
   const openBuyCreditsModal = () => {
     isOpen.value = true;
+    trackEvent("top_up_begin");
   };
 
   const closeBuyCreditsModal = () => {
